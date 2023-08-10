0954264f638aa8895c831ce93f1d8280a796f269
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-08-10 17:20:46 -0300

    style: exports

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import Header from './Header'
+import SearchIcon from './SearchIcon'
+import PrimaryInputWithSearchIcon from './PrimaryInput'
+import CartIcon from './CartIcon'
+import CartControl from './CartControl'
+
+export { Header, SearchIcon, PrimaryInputWithSearchIcon, CartIcon, CartControl }
