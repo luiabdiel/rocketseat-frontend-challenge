0fffec060edf86bc1426c87272f72b61cf1f3d91
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-08-16 22:21:44 -0300

    style: add grid template columns

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -3,12 +3,22 @@
 import { useProducts } from '@/hooks/useProducts'
 import React from 'react'
 import { ProductCard } from '.'
+import { styled } from 'styled-components'
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 256px);
+  grid-gap: 32px;
+  max-width: 100%;
+
+  margin-top: 32px;
+`
 
 const ProductsList = () => {
   const { data } = useProducts()
 
   return (
-    <div>
+    <ListContainer>
       {data?.map((product) => (
         <ProductCard
           key={product.id}
@@ -17,7 +27,7 @@ const ProductsList = () => {
           price={product.price_in_cents}
         />
       ))}
-    </div>
+    </ListContainer>
   )
 }
 
