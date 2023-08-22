80a2addcb85eedcf6addf04c57a0d54fca8a35fc
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-08-22 11:32:44 -0300

    feat: shopping cart summary section

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BackButton, CartItem, DefaultPageLayout } from '@/components'
+import { BackButton, CartItem, DefaultPageLayout, Divider } from '@/components'
 import { useLocalStorage } from '@/hooks/useLocalStorage'
 import { ProductInCart } from '@/types/ProductInCart'
 import { formatPrice } from '@/utils/Format'
@@ -9,20 +9,24 @@ import { styled } from 'styled-components'
 
 const Container = styled.div`
   display: flex;
-  align-items: flex-start;
   justify-content: center;
   flex-direction: column;
+  gap: 32px;
+
+  @media (min-width: ${(props) => props.theme.desktopBreakpoint}) {
+    flex-direction: row;
+  }
 `
 
 const CartListContainer = styled.div`
-  margin-top: 24px;
-
   h3 {
     font-weight: 500;
     font-size: 24px;
     line-height: 150%;
     text-transform: uppercase;
     color: var(--text-dark-2);
+
+    margin-top: 24px;
   }
 
   p {
@@ -47,6 +51,56 @@ const CartList = styled.ul`
   margin-top: 24px;
 `
 
+const CartResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  background-color: var(--white);
+
+  min-width: 352px;
+  padding: 16px 24px;
+
+  h3 {
+    font-weight: 600;
+    font-size: 20px;
+    color: var(--text-dark-2);
+    text-transform: uppercase;
+
+    margin-bottom: 30px;
+  }
+`
+
+const TotalItem = styled.div<{ isBold: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+
+  font-weight: ${(props) => (props.isBold ? '600' : '400')};
+  font-size: 16px;
+  line-height: 150%;
+
+  margin-bottom: 12px;
+`
+
+const ShopBtn = styled.button`
+  color: var(--white);
+  text-transform: uppercase;
+
+  border-radius: 4px;
+  padding: 12px;
+  border: none;
+  margin-top: 40px;
+
+  width: 100%;
+
+  background-color: var(--success-color);
+  cursor: pointer;
+`
+
 const CartPage = () => {
   const { value, updateLocalStorage } = useLocalStorage<ProductInCart[]>(
     'cart-items',
@@ -61,6 +115,8 @@ const CartPage = () => {
   }
 
   const cartTotal = formatPrice(calculateTotal(value))
+  const deliveryFee = 4000
+  const cartTotalWithDelivery = formatPrice(calculateTotal(value) + deliveryFee)
 
   const handleUpdateQuantity = (id: string, quantity: number) => {
     const newValue = value.map((item) => {
@@ -80,8 +136,8 @@ const CartPage = () => {
   return (
     <DefaultPageLayout>
       <Container>
-        <BackButton navigate="/" />
         <CartListContainer>
+          <BackButton navigate="/" />
           <h3>Seu carrinho</h3>
           <p>
             Total {value.length} produtos
@@ -98,6 +154,23 @@ const CartPage = () => {
             ))}
           </CartList>
         </CartListContainer>
+        <CartResultContainer>
+          <h3>Resumo do pedido</h3>
+          <TotalItem isBold={false}>
+            <p>Subtotal do produtos</p>
+            <p>{cartTotal}</p>
+          </TotalItem>
+          <TotalItem isBold={false}>
+            <p>Entrega</p>
+            <p>{formatPrice(deliveryFee)}</p>
+          </TotalItem>
+          <Divider />
+          <TotalItem isBold>
+            <p>Total</p>
+            <p>{cartTotalWithDelivery}</p>
+          </TotalItem>
+          <ShopBtn>Finalizar compra</ShopBtn>
+        </CartResultContainer>
       </Container>
     </DefaultPageLayout>
   )
